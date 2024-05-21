16c3ab54a0338f21ca3e14be6f0b51b956d21c04
kapila-dhilipan <120449602+kapila-dhilipan@users.noreply.github.com>
2024-05-21 14:34:43 +0530

    Update index.js
    code commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello third !\n'
+  const msg = 'Hello kapila !\n'
   res.end(msg);
 });
 
